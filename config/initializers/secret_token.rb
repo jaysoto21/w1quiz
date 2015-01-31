130d147e17fd60ad9131f6e0b0b88d259c2f4c0d
Jay Soto <jaysoto21@gmail.com>
2015-01-31 06:53:13 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+W1Quiz::Application.config.secret_key_base = 'fb4b5b4323eee483f42922eed9567d3e43dd9e4bf9d8b985a798e64e8bfcdf80c6ceb40df19e14dd7f456bc5553fec6f7f0ea13425609e76826cb93732449b35'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+W1Quiz::Application.config.session_store :cookie_store, key: '_W1Quiz_session'
